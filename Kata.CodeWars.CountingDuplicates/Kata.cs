2b2adf3f09c78bb9f84370e216c7c23aa72d0187
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-08-23 22:32:18 -0500

    Add project files.

---
+++ Kata.CodeWars.CountingDuplicates/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.CodeWars.CountingDuplicates
+{
+    public class Kata
+    {
+        public static int DuplicateCount(string str)
+        {
+            var characters = str.ToLower().ToArray();
+            var pivots = str.ToLower().ToArray().Distinct();
+            int i = 0;
+            foreach (char pivot in pivots)
+            {
+                var exist = characters.Where(characer => characer == pivot).Count() >= 2;
+                if (exist)
+                    i++;
+            }
+            return i;
+        }
+    }
+}
